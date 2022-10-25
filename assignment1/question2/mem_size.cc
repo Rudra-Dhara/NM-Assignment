7d24dd2cef9f325879fbcf9c93a8f035734ae9bc
Rudra-Dhara <rudra.dhara1141@gmail.com>
2022-10-26 01:41:00 +0545

    Q2 atmpt1

---
+++ assignment1/question2/mem_size.cc
@@ -0,0 +1,14 @@
+/*Program to find the memory size of datatypes int,
+float, bool, and char*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Size of int is "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"Size of int is "<<sizeof(float)<<" bytes"<<endl;
+    cout<<"Size of int is "<<sizeof(bool)<<" bytes"<<endl;
+    cout<<"Size of int is "<<sizeof(char)<<" bytes"<<endl;
+    
+    return 0;
+}
