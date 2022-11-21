f52f28295217e484b2db209415b5c70d57e3cdf1
Rudra-Dhara <rudra.dhara1141@gmail.com>
2022-11-22 00:24:22 +0545

    finally well

--- assignment2/Q2_data_interpolation/lag_intpol_data.py
+++ assignment2/Q2_data_interpolation/lag_intpol_data.py
@@ -5,6 +5,7 @@ sys.path.append('..')
 
 import Self_defined_functions.int_function as ifun
 import Self_defined_functions.lag_interpole_fn as lag
+import Self_defined_functions.root_fn as rt
 
 
 a,b=np.loadtxt("data.dat",unpack=True)
@@ -16,12 +17,28 @@ x=2.5
 print("The value of the function at x = {} is {}".format(x,lag.Lag_fn(x,a,b)))
 
 print("\n\n############\nPart b")
+#part b
+
+x_tol=0.0001
+x_start1=0
+x_start2=3 
+print("The first root is: ",rt.NR_rt(lag.Lag_fn,lag.Lag_diff,x_start1,x_tol,a,b))
+print("The first root is: ",rt.NR_rt(lag.Lag_fn,lag.Lag_diff,x_start2,x_tol,a,b))
 
 
-x=np.linspace(-2.5,2.87,1000)
+
+
+
+
+
+
+
+x=np.linspace(-4,4,100)
+
 
 plt.plot(x,lag.Lag_fn(x,a,b),label="Interpolation")
-#plt.plot(lag.Lag_diff(x,a,b),label="lag_differentiation")
+plt.plot(x,lag.Lag_diff(x,a,b),label="lag_differentiation")
+plt.scatter(a,b)
 plt.legend()
 plt.grid()
 plt.show()
