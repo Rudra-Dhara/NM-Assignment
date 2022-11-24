257ce4dce9616db170f2e43d92b686696482b9c3
Rudra-Dhara <rudra.dhara1141@gmail.com>
2022-11-24 22:54:22 +0545

    order approximation

--- assignment2/Q2_data_interpolation/lag_intpol_data.py
+++ assignment2/Q2_data_interpolation/lag_intpol_data.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import math as m
 sys.path.append('..')
 
 #importing self defined functions
@@ -10,6 +11,9 @@ import Self_defined_functions.root_fn as rt
 
 
 a,b=np.loadtxt("data.dat",unpack=True)
+
+a=np.array(a)
+b=np.array(b)
 
 #part a
 # the function is defined by the lag.Lag_fn(x,a,b) teke the a, b list and x is the variable
@@ -46,12 +50,12 @@ def F(x,a,b):
     n_div=100
     return ifun.int_simp(lag.Lag_fn,0,x,n_div,a,b)
 
-nr_root=rt.NR_rt(F,lag.Lag_fn,-1,0.000001,a,b)
-root_list=[nr_root]
-x=-1
-count=0 
-print("\n\n########\nPart d")
-while (x<=2):
+#nr_root=rt.NR_rt(F,lag.Lag_fn,-1,0.000001,a,b)
+#root_list=[nr_root]
+#x=-1
+#count=0 
+#print("\n\n########\nPart d")
+#while (x<=2):
     print("running...")             #the code take long time to run so useing the print
     x+=nr_root+1
     if abs(nr_root - rt.NR_rt(F,lag.Lag_fn,x,0.000001,a,b))> 0.00001:
@@ -62,7 +66,7 @@ while (x<=2):
     
 
 
-print("The the values of x where the integration is 0, list of the roots:\n",root_list)
+#print("The the values of x where the integration is 0, list of the roots:\n",root_list)
 
 #part e
 
@@ -70,3 +74,66 @@ coeff=lag.Lag_coeff(a,b)
 
 print("\n\n########\nPart e")
 print("The coefficient of x**0, x**1, x**2, x**3 are given by the list:\n",coeff)
+
+global c1
+c1 = coeff[0]
+k1 = coeff[1]
+k2 = coeff[2]
+k3 = coeff[3]
+
+def F_om(x,c,k2,k3):
+
+    return (c*x**3)/6 + x*k2 - k3
+
+def f_om(x,c,k2,k3):
+
+    return (c*x**2)/2 + k2 +0*k3
+
+omega = rt.NR_rt(F_om,f_om,0,0.000001,c1,k2,k3)
+b1= k1 - c1*omega
+
+a1 = k2 + (c1*omega**2)/2 -k1*omega
+
+print("\nThe a,b,c,omega are:(first approxiamtion) \n",[a1,b1,c1,omega])
+
+#now condider a,b,c are correct now we want to reestimate omega again
+# we use intarpolated value at x=1 to find the omega
+global lag_one
+lag_one = lag.Lag_fn(-1,a,b)
+def omega_od1(x,a1,b1,c1):
+    return (a1 - b1 + c1)*m.exp(-x) - lag_one
+
+def df_omega_od1(x,a1,b1,c1):
+    return -(a1 - b1 + c1)*m.exp(-1*x)
+#updated variavles
+omega1 = rt.NR_rt(omega_od1,df_omega_od1,-1,0.0000001,a1,b1,c1)
+
+b2= k1 - c1*omega1
+a2 = k2 + (c1*omega1**2)/2 -k1*omega1
+
+#2nd time approximation
+omega1 = rt.NR_rt(omega_od1,df_omega_od1,-1,0.0000001,a2,b2,c1)
+
+b2= k1 - c1*omega1
+a2 = k2 + (c1*omega1**2)/2 -k1*omega1
+print("\nThe updated a,b,c,omega are:(2nd approximation) \n",[a2,b2,c1,omega1])
+
+#3rd time approximation
+for i in range(10):
+    omega2 = rt.NR_rt(omega_od1,df_omega_od1,-1,0.0000001,a2,b2,c1)
+
+    b3= k1 - c1*omega1
+    a3 = k2 + (c1*omega1**2)/2 -k1*omega1
+
+
+def f_plt(x,a,b,c,omega):
+
+    return (a*x**2 + b*x + c)*np.exp(omega*x)
+
+plt.scatter(a,b)
+plt.plot(a,f_plt(a,a1,b1,c1,omega),label="first order")
+plt.plot(a,f_plt(a,a2,b2,c1,omega1),label="2nd order")
+plt.plot(a,f_plt(a,a3,b3,c1,omega2),label="3rd oredr")
+plt.legend()
+
+plt.show()
