acebb5b45d3bc8537d0b52d2b40e987b827aac78
Rudra-Dhara <rudra.dhara1141@gmail.com>
2022-11-25 05:23:32 +0545

    skipping the exam

--- assignment2/Q2_data_interpolation/lag_intpol_data.py
+++ assignment2/Q2_data_interpolation/lag_intpol_data.py
@@ -28,6 +28,9 @@ x_tol=0.0000001
 x_start1=0
 x_start2=3 
 
+root1=rt.NR_rt(lag.Lag_fn,lag.Lag_diff,x_start1,x_tol,a,b)
+root2=rt.NR_rt(lag.Lag_fn,lag.Lag_diff,x_start2,x_tol,a,b)
+
 print("\n\n############\nPart b")
 print("The first root is: ",rt.NR_rt(lag.Lag_fn,lag.Lag_diff,x_start1,x_tol,a,b))
 print("The second root is: ",rt.NR_rt(lag.Lag_fn,lag.Lag_diff,x_start2,x_tol,a,b))
@@ -81,81 +84,53 @@ k1 = coeff[1]
 k2 = coeff[2]
 k3 = coeff[3]
 
-def F_om(x,c,k2,k3):
+def F_om(x,c,k1,k2,k3):
 
-    return (c*x**3)/6 + x*k2 - k3
+    return (c*x**3)/6 - 0.5*k1*x**2 + x*k2 - k3
 
-def f_om(x,c,k2,k3):
+def f_om(x,c,k1,k2,k3):
 
-    return (c*x**2)/2 + k2 +0*k3
+    return (c*x**2)/2 - x*k1 + k2 +0*k3
 
-omega = rt.NR_rt(F_om,f_om,0,0.000001,c1,k2,k3)
+omega = rt.NR_rt(F_om,f_om,0.2,0.00000001,c1,k1,k2,k3)
 b1= k1 - c1*omega
 
-a1 = k2 + (c1*omega**2)/2 -k1*omega
+a1 = k2 - (c1*omega**2)/2 -b1*omega
 
-print("\nThe a,b,c,omega are:(first approxiamtion) \n",[a1,b1,c1,omega])
+print("\nThe a,b,c,omega are: \n",[a1,b1,c1,omega])
 
-#now condider a,b,c are correct now we want to reestimate omega again
-# we use intarpolated value at x=1 to find the omega
-global lag_one
-lag_one = lag.Lag_fn(-1,a,b)
-def omega_od1(x,a1,b1,c1):
-    return (a1 - b1 + c1)*m.exp(-x) - lag_one
+#another method to calculate part e
+#using the roots
+#we use roots of the equation as the root of quadratic equation as the quadratic equation has no roots
+# we have the closed form function of first derivative k1
+#we will use those to find the a,b,c,omega
+# we know the value of c
 
-def df_omega_od1(x,a1,b1,c1):
-    return -(a1 - b1 + c1)*m.exp(-1*x)
-#updated variavles
-omega1 = rt.NR_rt(omega_od1,df_omega_od1,-1,0.0000001,a1,b1,c1)
+a2= c1/(root1*root2)
 
-b2= k1 - c1*omega1
-a2 = k2 + (c1*omega1**2)/2 -k1*omega1
+b2 = -a2*(root2+root1)
 
-#updating k2 as 2nd order deriviative can contain some explicit errors
-
-k2 = a2 + b2*omega1 + c1*omega1**2/6
-#2nd time approximation
-omega1 = rt.NR_rt(omega_od1,df_omega_od1,-1,0.0000001,a2,b2,c1)
+omega2 = (k1 - b2)/c1
+print("\nWe have calculated the value from the roots for the function. Here we are getting the better value\n")
+print("With out using heigher order derivative, the value of a, b, c, omega:\n",[a2,b2,c1,omega2])
 
 
-print("\nThe updated a,b,c,omega are:(2nd approximation) \n",[a2,b2,c1,omega1])
-
-omega2 = rt.NR_rt(omega_od1,df_omega_od1,-1,0.0000001,a2,b2,c1)
-
-b3= k1 - c1*omega2
-a3 = k2 + (c1*omega2**2)/2 -k1*omega2
-
-#3rd time approximation
-for i in range(3):                          #as 3 parameter is updating more than 3 itteration is giving large errors
-    omega2 = rt.NR_rt(omega_od1,df_omega_od1,-1,0.0000001,a3,b3,c1)
-
-    b3= k1 - c1*omega2
-    a3 = k2 + (c1*omega2**2)/2 -k1*omega2
-    k2 = a3 + b3*omega2 + c1*omega2**2/6    
-
-#up to this point we consider the coefficient are well calculated except omega
-#so we want to improve the value of omega
 
 def f_plt(x,a,b,c,omega):
 
     return (a*x**2 + b*x + c)*np.exp(omega*x)
 
-print("after 3rd approximation we are getting the value of the coefficients are:\n",[a3,b3,c1,omega2])
-# 4th order approximation that only the value of a3 and omega2 have some errosr
 
-global lag_4
-lag_4 = lag.Lag_fn(-1,a,b)
-def omega_od1(x,a1,b1,c1):
-    return (a1 + b1 + c1)*m.exp(omega) - lag_4
+#now condider a,b,c are correct now we want to reestimate omega again
+# we use intarpolated value at x=1 to find the omega
 
-def df_omega_od1(x,a1,b1,c1):
-    return -(a1 - b1 + c1)*m.exp(-1*x)
 
- 
+
+
 
 plt.scatter(a,b)
-plt.plot(a,f_plt(a,a3,b3,c1,omega2),label="3rd oreder")
-plt.plot(a,f_plt(a,1,b3,c1,0.2),label="3rd")
+plt.plot(a,f_plt(a,a1,b1,c1,omega),label="with forward difference approximation",color='red')
+plt.plot(a,f_plt(a,a2,b2,c1,omega2),label="using the rootsroots",color="black")
 plt.legend()
 
 plt.show()
